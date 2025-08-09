08ad607e5b4e657567a751a70f2b0872fad9d461
thetanish1 <tanishdewase25@gmail.com>
2025-08-09 11:56:12 +0530

    [change] in app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Tanish React App!
+          This is The Tanish React App!
         </p>
         <a
           className="App-link"
